cba092ae1a5ffa5a49e1a5873b39ac26c07d3221
V Yogaa sakthi sree <yogaa.sakthi.btech@gmail.com>
2025-10-26 19:05:32 +0530

    Add files via upload

---
+++ ifelse.py
@@ -0,0 +1,21 @@
+if(True):
+    print("yes")
+else:
+    print("no")
+
+a="apple"
+if(a=="apple"):
+    print("ok")
+else:
+    print("not ok")
+
+a=int(input("enter a number"))
+if(a%3==0 and a%5==0):
+    print("Eligible")
+else:
+    print("Not Eligible")
+score=int(input())
+if(score<35):
+    print("Fail")
+else:
+    print("Pass")
