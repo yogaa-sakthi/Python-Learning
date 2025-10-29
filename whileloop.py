e147b3b460e6e8ff746ad0528e650283da36e6e3
V Yogaa sakthi sree <yogaa.sakthi.btech@gmail.com>
2025-10-29 18:03:37 +0530

    Add files via upload

---
+++ whileloop.py
@@ -0,0 +1,26 @@
+i=0
+while(i==0):
+    print(i)
+    i=1;
+
+
+i=1
+while(i<5):
+    print(i)
+    i=i+1;
+
+
+
+
+i=10
+while(i>0):
+    print(i,end=",")
+    i=i-1;
+
+
+i=3
+fact=1
+while(i>0):
+    fact=fact*i
+    i=i-1;
+print(fact)    
